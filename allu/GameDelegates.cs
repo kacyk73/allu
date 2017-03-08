6f6a95aed5461fd9b4ad402235c37bbe309df95e
kacyk73 <pk@koczyk.pl>
2017-03-08 18:37:42 +0100

    delegate created

---
+++ allu/GameDelegates.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace allu
+{
+    public class GameDelegates
+    {
+        public delegate void ArmyChangedHandler(int i, float j);
+    }
+}
