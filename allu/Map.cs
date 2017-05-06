60d11221817c39243559740308e1d8265949c2a4
kacyk73 <pk@koczyk.pl>
2017-05-06 15:24:12 +0200

    minor changes

--- allu/Map.cs
+++ allu/Map.cs
@@ -16,7 +16,7 @@ namespace allu
         //army dictionary
         public Dictionary<PosXY, Army> ArmyUnits = new Dictionary<PosXY, Army>();
 
-        //army helper array for performance purposes 1237
+        //army helper array for performance purposes 12372
         private bool[,] isArmy;
 
         public bool[,] IsArmy
