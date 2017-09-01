deaff43d2fcb0b78525a77b16cf3eaf65a790306
kacyk73 <pk@koczyk.pl>
2017-09-02 00:43:07 +0200

    minor

--- allu/Map.cs
+++ allu/Map.cs
@@ -16,7 +16,7 @@ namespace allu
         //army dictionary
         public Dictionary<PosXY, Army> ArmyUnits = new Dictionary<PosXY, Army>();
 
-        //army helper array for performance purposes 127255e3ddfjddsdrrde66fgggsfhgddgg
+        //army helper array for performance purposes 127255e3dfjddsdrrde66fgggsfhgddgg
         private bool[,] isArmy;
 
         public bool[,] IsArmy
