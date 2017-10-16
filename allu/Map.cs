e592a7165f0192eb10ca9f44732195a7176242c6
kacyk73 <pk@koczyk.pl>
2017-10-16 16:55:41 +0200

    consistency fixed

--- allu/Map.cs
+++ allu/Map.cs
@@ -16,12 +16,12 @@ namespace allu
         //army dictionary
         public Dictionary<PosXY, Army> ArmyUnits = new Dictionary<PosXY, Army>();
 
-        //army helper array for performance purposes 1233sfdf
+        //army helper array for performance purposes 1233sfd
 
         public bool[,] IsArmy
         {
-            get { return isArmy; }
-            set { isArmy = value; }
+            get { return IsArmy; }
+            set { IsArmy = value; }
         }
 
 
